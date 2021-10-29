ef87030e336d88713763449f950b7a6149a0eef3
JimZAH <james@colderwood.com>
2021-10-29 15:32:56 +0100

    Valid codes stored in EEPROM

--- TSACS_LASER_TAG.ino
+++ TSACS_LASER_TAG.ino
@@ -1,21 +1,26 @@
+#include "EEPROM.h"
 #include <IRremote.h>
 
-#define ADDRESS 0x01
+#define DEBUG 0
+#define CODE_COUNT_ADDRESS 0x00
 #define EXIT 0x065
 #define HELP 0x068
 #define LIST 0x06C
+#define MAX_CODES 0x0A
 #define PROGRAM 8
 #define RECEIVER 2
 #define RETURN 0x0A
 #define WRITE 0x077
 
-bool alert = false;
+int ADDRESS = CODE_COUNT_ADDRESS;
+int RADDRESS = CODE_COUNT_ADDRESS;
 bool alarm = false;
 int bytes = 0;
+byte cc = 0;
 uint8_t input[30];
 int lc = 0;
 int fw_version = 1;
-bool learn = true;
+bool learn = false;
 int n = 0;
 int occ[4][4]={
     {0,0},
@@ -26,8 +31,18 @@ int occ[4][4]={
 bool program_mode = false;
 int samples[4] = {0,0,0,0};
 long tc = 0;
-long timer = 1500000;
-int valid_codes[3] = {521,537,57};
+long timer = 750000;
+int valid_codes[10] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0};
 int wn = 0;
 
 
@@ -37,7 +52,7 @@ decode_results results;
 
 
 void setup() {
-  
+  //EEPROM.update(0,0);
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(PROGRAM, INPUT_PULLUP);
   pinMode(4, OUTPUT);
@@ -45,7 +60,7 @@ void setup() {
   
   digitalWrite(3, HIGH);
   digitalWrite(4, LOW);
-
+  
   Serial.begin(9600);
 
   if (digitalRead(8) == LOW){
@@ -57,9 +72,20 @@ void setup() {
   Serial.print("READY\n");
 }
 
-void loop() {
-  // Incoming codes
-  if (learn){
+
+// Allows user to marry laser gun with target.
+
+void learnCodes(){
+   Serial.println("Current stored codes: ");
+    int csc = 0;
+    for (int i=0; i<=9; i++){
+      RADDRESS+=2;
+     csc = readCodes(RADDRESS);
+      Serial.print(RADDRESS);
+      Serial.print(": ");
+      Serial.println(csc, DEC);
+    }
+    RADDRESS = CODE_COUNT_ADDRESS;
     while (1){
       if (irrecv.decode(&results)){
         Serial.print("LEARNING: ");
@@ -88,40 +114,68 @@ void loop() {
     } else if (occ[2][1] > occ[3][1] || occ[2][1] > occ[0][1]){
         wn = occ[2][0];
     }else if (occ[0][0] == occ[1][0] && occ[1][0] == occ[2][0] && occ[3][0] == occ[0][0]){
-        wn = occ[1][0];
+        wn = occ[0][0];
     } else {
-        Serial.println("Equal compare: Try again");
         wn = 0;
     }
     lc=0;
     Serial.println(wn);
+    // EEPROM STORE
+    EEPROM.get(CODE_COUNT_ADDRESS, cc);
+    if (cc >= MAX_CODES){
+      Serial.println("ERROR: You have reach the maximum stored valid codes!"); //TODO: Give user option to clear these.
+    } else {
+      //TODO: EEPROM is not inc on code store
+      Serial.print("Current code count: ");
+      ADDRESS += 2;
+      writeCodes(ADDRESS, wn);
+      cc++;
+      EEPROM.put(CODE_COUNT_ADDRESS, cc);
+      Serial.println(cc,DEC);
+    }
+}
+
+
+void loop() {
+
+  // Learn new codes
+  while (learn){
+    learnCodes();
   }
+
+  // Grab valid codes from memory.
+  if (valid_codes[0] == 0){
+    for (int i=0; i<=10; i++){
+      valid_codes[i] = readCodes(ADDRESS+=2);
+      Serial.print(i);
+      Serial.print(" CODES: ");
+      Serial.println(valid_codes[i]);
+    }
+  }
+
+  
   if (irrecv.decode(&results)){
-    Serial.println(results.value);
   for (int i=0; i <= sizeof(valid_codes)/2; i++){
-      if (results.value == int(valid_codes[i])){
-        Serial.print("OK : ");
-        Serial.println(valid_codes[i]);
-        alert = true;
+      if (results.value == valid_codes[i] && !alarm){
+        alarm = true;
+        tc=0;
         digitalWrite(LED_BUILTIN,HIGH);
       }
     }
     irrecv.resume();
   } 
- 
-  if (alert && !alarm){
-    alarm = true;
-    tc = 0;
-  } else if (alert && alarm){
+
+  // Alarm activated 
+  if (alarm){
     tc++;
     if (tc > timer){
       alarm = false;
-      alert = false;
       digitalWrite(LED_BUILTIN,LOW);
     }
   }
 }
 
+// Serial programming mode
 void program(){
   
   Serial.print("Welcome to the TSACS target machine\n(C) James Colderwood 2021 Version: ");
@@ -158,5 +212,24 @@ void program(){
     }
     
   }
+
+}
+
+int readCodes(int address){
   
+  byte b1 = EEPROM.read(address);
+  byte b2 = EEPROM.read(address+1);
+  
+  return (b1 << 8) + b2;
+  
+}
+
+void writeCodes(int address, int number){
+
+    byte b1 = (number >> 8) & 0xFF;
+    byte b2 = number & 0xFF;
+    
+    EEPROM.update(address, b1);
+    EEPROM.update(address+1, b2);
+    
 }
